77c86099d4bf4568c867cdd1c7b66fd2c63ad2b4
leo <lionleogary@gms.tku.edu.tw>
2019-04-04 21:01:41 +0800

    UP

--- app/src/main/java/com/example/editteext/MainActivity.java
+++ app/src/main/java/com/example/editteext/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.editteext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int size=30;
+    public void response(View v){
+        EditText ed1,ed2,ed3;
+        TextView txv;
+
+        ed1=findViewById(R.id.editText);
+        ed2=findViewById(R.id.editText2);
+        ed3=findViewById(R.id.editText3);
+        txv=findViewById(R.id.textview);
+        txv.setTextSize(size);
+        txv.setText(ed1.getText().toString()+",您好\n"+"您的BMI是:");
+
+
     }
 }
